b0b207979bb9671085248afbc2dd1485b44c6b1f
Nick Johnson <nlj222@gmail.com>
2023-01-18 10:02:40 -0800

    lab complete

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 let addToy = false;
 
+let url = "http://localhost:3000/toys"
+
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
@@ -12,4 +14,96 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
+
+
 });
+
+const toyCollection = document.querySelector('#toy-collection');
+
+function renderToyData(toyData){
+  const cardDiv = document.createElement("div");
+  cardDiv.classList.add("card");
+  const likes = document.createElement("p");
+  likes.textContent = `${toyData.likes} likes`;
+  const toyName = document.createElement("h2");
+  toyName.textContent = toyData.name;
+  const image = document.createElement("img");
+  image.setAttribute("src", toyData.image);
+  image.classList.add("toy-avatar");
+  const button = document.createElement("button");
+  button.classList.add("like-btn");
+  button.dataset.id = toyData.id;
+  button.textContent = "Like ❤️";
+
+  button.addEventListener("click", (e) => {
+    console.log("click");
+    fetch(`${url}/${toyData.id}`, {
+      method: "PATCH",
+      headers:
+        {
+          "Content-Type": "application/json",
+          Accept: "application/json"
+        },
+        body: JSON.stringify({
+          "likes": parseInt(toyData.likes = toyData.likes + 1)
+        })
+    })
+    .then(response => response.json())
+    .then(likes.textContent = `${toyData.likes} likes`)
+  })
+
+  toyCollection.appendChild(cardDiv);
+
+  cardDiv.append(toyName, image, likes, button);
+}
+
+fetch(url)
+.then(response => response.json())
+.then(toyData => {
+  console.log(toyData);
+  toyData.forEach(renderToyData);
+})
+
+const createToyBtn = document.querySelector(".add-toy-form");
+console.log(createToyBtn);
+
+createToyBtn.addEventListener("submit", (e) => {
+  e.preventDefault();
+  // debugger;
+  fetch(url, {
+    method: "POST",
+    headers:
+      {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+    body: JSON.stringify({
+      "name": e.target.name.value,
+      "image": e.target.image.value,
+      "likes": parseInt(0)
+    })
+  })
+  .then(res => res.json())
+  .then(data => renderToyData(data))
+})
+
+
+
+// const likeBtn = document.querySelector(".like-btn");
+// console.log(likeBtn);
+
+// likeBtn.addEventListener("click", (e) => {
+//   console.log("click");
+//   fetch(url + toys.id, {
+//     method: "PATCH",
+//     headers:
+//       {
+//         "Content-Type": "application/json",
+//         Accept: "application/json"
+//       },
+//       body: JSON.stringify({
+//         "likes": parseInt(e.target.likes++)
+//       })
+//   })
+// })
